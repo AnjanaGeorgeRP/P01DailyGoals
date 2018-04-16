03ca109d746c7246a868736766d401cc1a743e78
AnjanaGeorge <16004097@myrp.edu.sg>
2018-04-16 18:24:59 +0800

    Enhanced App

--- app/src/main/java/com/myapplicationdev/android/p01_dailygoals/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p01_dailygoals/MainActivity.java
@@ -1,6 +1,8 @@
 package com.myapplicationdev.android.p01_dailygoals;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,19 +14,25 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText et;
+    RadioGroup rg1;
+    RadioGroup rg2;
+    RadioGroup rg3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button btn = (Button) findViewById(R.id.button);
+        et = (EditText) findViewById(R.id.editText);
+        // Get the RadioGroup object
+        rg1 = (RadioGroup) findViewById(R.id.radioGroup1);
+        rg2 = (RadioGroup) findViewById(R.id.radioGroup2);
+        rg3 = (RadioGroup) findViewById(R.id.radioGroup3);
+
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View arg0) {
-                // Get the RadioGroup object
-                RadioGroup rg1 = (RadioGroup) findViewById(R.id.radioGroup1);
-                RadioGroup rg2 = (RadioGroup) findViewById(R.id.radioGroup2);
-                RadioGroup rg3 = (RadioGroup) findViewById(R.id.radioGroup3);
 
                 // Get the Id of the selected radio button in the RadioGroup
                 int selectedButtonId1 = rg1.getCheckedRadioButtonId();
@@ -36,9 +44,6 @@ public class MainActivity extends AppCompatActivity {
                 RadioButton rb2 = (RadioButton) findViewById(selectedButtonId2);
                 RadioButton rb3 = (RadioButton) findViewById(selectedButtonId3);
 
-                // Get the EditText that user keys
-                EditText et = (EditText) findViewById(R.id.editText);
-
                 // Create an intent to start another activity
                 Intent intent = new Intent(getBaseContext(), ResultActivity.class);
                 String[] info = {rb1.getText().toString(),rb2.getText().toString(),rb3.getText().toString(),et.getText().toString()};
@@ -46,6 +51,36 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
             }});
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        String strRef = et.getText().toString();
+        int radio1 = rg1.getCheckedRadioButtonId();
+        int radio2 = rg2.getCheckedRadioButtonId();
+        int radio3 = rg3.getCheckedRadioButtonId();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        prefEdit.putString("reflection",strRef);
+        prefEdit.putInt("radio1",radio1);
+        prefEdit.putInt("radio2",radio2);
+        prefEdit.putInt("radio3",radio3);
+        prefEdit.commit();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String strRef = prefs.getString("reflection","");
+        int radio1 = prefs.getInt("radio1",1);
+        int radio2 = prefs.getInt("radio2",1);
+        int radio3 = prefs.getInt("radio3",1);
+        et.setText(strRef);
+        rg1.check(radio1);
+        rg2.check(radio2);
+        rg3.check(radio3);
     }
 }
--- app/src/main/java/com/myapplicationdev/android/p01_dailygoals/ResultActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p01_dailygoals/ResultActivity.java
@@ -3,6 +3,8 @@ package com.myapplicationdev.android.p01_dailygoals;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -17,8 +19,18 @@ public class ResultActivity extends AppCompatActivity {
         String[] info = i.getStringArrayExtra("info");
         // Get the TextView object
         TextView tv1 = (TextView) findViewById(R.id.textView);
+        Button back = (Button) findViewById(R.id.back);
+
         // Display the name and age on the TextView
         tv1.setText("Read up on materials before class: " + info[0] + "\n Arrive on time soa as not to miss impoertant part of the lesson: " + info[1]+"\n Attempt the problem myself:"+info[2]+"\n Relection:"+info[3]);
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(intent);
+
+            }});
     }
+
 
 }
